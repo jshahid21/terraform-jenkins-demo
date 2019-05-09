1c4a6badb6f7901022ba8c314251234be03e9864
jshahid21 <jshahid21@github.com>
2019-05-09 13:15:44 -0400

    .

--- datasources.tf
+++ datasources.tf
@@ -22,6 +22,6 @@ data "oci_core_vnic" "vnic" {
     vnic_id = ["${lookup(data.oci_core_vnic_attachments.vnics.vnic_attachments[0],"vnic_id")}"]
 }
 
-output "public_ip" {
-    value = ["${oci_core_vnic.vnic.public_ip_address}"]
+output "instance_ip_address" {
+    value = ["${data.oci_core_vnic.public_ip_address}"]
 }
