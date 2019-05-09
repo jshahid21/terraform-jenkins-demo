3a6c60f58a3758c0f701723276dd9f0da697ca9f
jshahid21 <jshahid21@github.com>
2019-05-09 13:08:53 -0400

    .

--- datasources.tf
+++ datasources.tf
@@ -19,7 +19,7 @@ data "oci_core_vnic_attachments" "vnics" {
 
 # Gets the OCID of the first (default) vNIC
 data "oci_core_vnic" "vnic" {
-    vnic_id = "${lookup(data.oci_core_vnic_attachments.vnics.vnic_attachments[0],"vnic_id")}"
+    vnic_id = ["${lookup(data.oci_core_vnic_attachments.vnics.vnic_attachments[0],"vnic_id")}"]
 }
 
 output "public_ip" {
