bfaa88e1c2a74dbded4d8f476f052cd20b5462b4
jshahid21 <jshahid21@github.com>
2019-05-09 11:26:35 -0400

    update

--- datasources.tf
+++ datasources.tf
@@ -10,14 +10,14 @@ data "oci_identity_availability_domains" "ADs" {
 # # }
 #
 #
-# # Gets a list of vNIC attachments on the instance
-# data "oci_core_vnic_attachments" "InstanceVnics" {
-#     compartment_id = "${var.compartment_ocid}"
-#     availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
-#     instance_id = "${oci_core_instance.tajVM.id}"
-# }
-# #
-# # Gets the OCID of the first (default) vNIC
-# data "oci_core_vnic" "InstanceVnic" {
-#     vnic_id = "${lookup(data.oci_core_vnic_attachments.InstanceVnic.vnic_attachments[0],"vnic_id")}"
-# }
+# Gets a list of vNIC attachments on the instance
+data "oci_core_vnic_attachments" "InstanceVnics" {
+    compartment_id = "${var.compartment_ocid}"
+    availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
+    instance_id = "${oci_core_instance.tajVM.id}"
+}
+#
+# Gets the OCID of the first (default) vNIC
+data "oci_core_vnic" "InstanceVnic" {
+    vnic_id = "${lookup(data.oci_core_vnic_attachments.InstanceVnic.vnic_attachments[0],"vnic_id")}"
+}
