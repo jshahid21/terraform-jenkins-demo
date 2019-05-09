baeb367903cdc2ae986572ba7a213a715d2af041
jshahid21 <jshahid21@github.com>
2019-05-09 11:27:24 -0400

    .

--- datasources.tf
+++ datasources.tf
@@ -1,16 +1,16 @@
-Gets a list of Availability Domains
-data "oci_identity_availability_domains" "ADs" {
-    compartment_id = "${var.tenancy_ocid}"
-}
-#
-# # data "oci_core_security_lists" "tajSL" {
-# #     #Required
-# #     compartment_id = "${var.compartment_ocid}"
-# #     vcn_id = "${oci_core_virtual_network.tajVCN.id}"
-# # }
-#
-#
-# Gets a list of vNIC attachments on the instance
+# Gets a list of Availability Domains
+# data "oci_identity_availability_domains" "ADs" {
+#     compartment_id = "${var.tenancy_ocid}"
+# }
+# #
+# # # data "oci_core_security_lists" "tajSL" {
+# # #     #Required
+# # #     compartment_id = "${var.compartment_ocid}"
+# # #     vcn_id = "${oci_core_virtual_network.tajVCN.id}"
+# # # }
+# #
+# #
+# # Gets a list of vNIC attachments on the instance
 data "oci_core_vnic_attachments" "InstanceVnics" {
     compartment_id = "${var.compartment_ocid}"
     availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
