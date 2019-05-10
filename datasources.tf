d3b557ec7230be5ff8820b7576b66ba21fce0fac
jshahid21 <jshahid21@github.com>
2019-05-09 21:22:16 -0400

    add compute instance

--- datasources.tf
+++ datasources.tf
@@ -10,7 +10,7 @@ data "oci_identity_availability_domains" "ADs" {
 # }
 #
 #
-Gets a list of vNIC attachments on the instance
+# Gets a list of vNIC attachments on the instance
 data "oci_core_vnic_attachments" "vnics" {
     compartment_id = "${var.compartment_ocid}"
     availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
