01c6794202ccced5c900e447dfc80aa9cf780c87
jshahid21 <jshahid21@github.com>
2019-07-11 16:19:18 -0400

    update

--- datasources.tf
+++ datasources.tf
@@ -9,15 +9,14 @@ data "oci_core_security_lists" "tajSL" {
     vcn_id = "${oci_core_virtual_network.tajVCN.id}"
 }
 
-#
-Gets a list of vNIC attachments on the instance
+#Gets a list of vNIC attachments on the instance
 data "oci_core_vnic_attachments" "vnics" {
     compartment_id = "${var.compartment_ocid}"
     availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
     instance_id = "${oci_core_instance.tajVM.id}"
 }
 
-# Gets the OCID of the first (default) vNIC
+#Gets the OCID of the first (default) vNIC
 data "oci_core_vnic" "vnic" {
     vnic_id = "${lookup(data.oci_core_vnic_attachments.vnics.vnic_attachments[0],"vnic_id")}"
 }
