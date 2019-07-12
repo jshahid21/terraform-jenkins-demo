c453fd95eb75ae7573116b8276f6cb50e9a1254b
jshahid21 <jshahid21@github.com>
2019-07-12 14:22:13 -0400

    update

--- compute.tf
+++ compute.tf
@@ -1,30 +1,30 @@
-resource "oci_core_instance" "tajVM" {
-  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
-  compartment_id = "${var.compartment_ocid}"
-  display_name = "APEXinstance"
-  shape = "${var.InstanceShape}"
-  state = "RUNNING"
-
-  source_details {
-      source_type = "image"
-      source_id = "${var.InstanceImageOCID[var.region]}"
-    }
-
-  create_vnic_details {
-    subnet_id = "${oci_core_subnet.tajSN.id}"
-    display_name = "primaryvnic"
-    assign_public_ip = true
-    hostname_label = "tajVM"
-  }
-
-  freeform_tags = {"DEMO"= "DEMO"}
-
-  metadata {
-    ssh_authorized_keys = "${var.ssh_public_key}"
-    user_data = "${base64encode(file(var.BootStrapFile))}"
-  }
-
-  timeouts {
-    create = "60m"
-  }
-}
+# resource "oci_core_instance" "tajVM" {
+#   availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
+#   compartment_id = "${var.compartment_ocid}"
+#   display_name = "APEXinstance"
+#   shape = "${var.InstanceShape}"
+#   state = "RUNNING"
+#
+#   source_details {
+#       source_type = "image"
+#       source_id = "${var.InstanceImageOCID[var.region]}"
+#     }
+#
+#   create_vnic_details {
+#     subnet_id = "${oci_core_subnet.tajSN.id}"
+#     display_name = "primaryvnic"
+#     assign_public_ip = true
+#     hostname_label = "tajVM"
+#   }
+#
+#   freeform_tags = {"DEMO"= "DEMO"}
+#
+#   metadata {
+#     ssh_authorized_keys = "${var.ssh_public_key}"
+#     user_data = "${base64encode(file(var.BootStrapFile))}"
+#   }
+#
+#   timeouts {
+#     create = "60m"
+#   }
+# }
