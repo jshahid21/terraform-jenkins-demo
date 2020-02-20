277157ef956ca78c2d05211238e3a07c57a4494a
jshahid21 <42783926+jshahid21@users.noreply.github.com>
2020-02-20 12:48:39 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "oci" {
-  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaa5txfxrudc5ezitovlpjznw6b3l6sqvkzddepsfu4es2i4x2xyeia"
-  user_ocid = "ocid1.user.oc1..aaaaaaaagf2r3i3id6lasstuhcuaydr7zg6g2ww4swujpiqrinj4ukrywaoq"
-  fingerprint = "00:16:9a:4b:c6:99:bc:10:ca:25:cb:0e:2f:5b:bd:87"
-  private_key_path = "/var/lib/jenkins/.ocijs/oci_api_key_js.pem"
-  region = "us-ashburn-1"
+  tenancy_ocid = ""
+  user_ocid = ""
+  fingerprint = ""
+  private_key_path = ""
+  region = ""
 }
